9ba0c9a47e1a134c9161cdcaed647c176925ead3
Casey Ferebee <cwferebee@gamil.com>
2023-06-27 13:59:59 -0500

    getting closer

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -8,6 +8,9 @@ export const ApplicationViews = () => {
 	const loggedInUser = JSON.parse(localStorage.getItem("bowler_user"));
 	const [startDate, setStartDate] = useState(new Date());
 	const selectedDate = startDate.toLocaleDateString();
+	const [cSelected, setCSelected] = useState([]);
+	const [selectedFriend, setSelectedFriend] = useState(null)
+
 	const [gameObj, setGameObj] = useState({
         id: null,
         userId: loggedInUser.id,
@@ -16,15 +19,19 @@ export const ApplicationViews = () => {
         date: new Date().toISOString(),
     });
 	
+	const handleFriendClick = (friendId) => {
+		setSelectedFriend(friendId)
+	}
+
 	return <>
 		<h1 className="title--main">300-Pins</h1>
 		<div>Let's Bowl</div>
 
 		<SubmitForm selectedHouse={selectedHouse} setSelectedHouse={setSelectedHouse}  setGameObj={setGameObj} gameObj={gameObj} selectedDate={selectedDate} startDate={startDate} setStartDate={setStartDate} />
 
-		<FriendsList selectedHouse={selectedHouse} setSelectedHouse={setSelectedHouse}  setGameObj={setGameObj} gameObj={gameObj} />
+		<FriendsList selectedHouse={selectedHouse} setSelectedHouse={setSelectedHouse}  setGameObj={setGameObj} gameObj={gameObj} cSelected={cSelected} setCSelected={setCSelected} onFriendClick={handleFriendClick} />
 
-		<ScoresView selectedHouse={selectedHouse} setSelectedHouse={setSelectedHouse}  setGameObj={setGameObj}  gameObj={gameObj} selectedDate={selectedDate}  startDate={startDate}  setStartDate={setStartDate}      />
+		<ScoresView selectedHouse={selectedHouse} setSelectedHouse={setSelectedHouse}  setGameObj={setGameObj}  gameObj={gameObj} selectedDate={selectedDate}  startDate={startDate}  setStartDate={setStartDate} cSelected={cSelected} setCSelected={setCSelected} selectedFriend={selectedFriend} />
 
 	</>
 }
